86216ab7176a628c0099452eda6ea018e63a9f55
PardeshiYogita02 <yogitaspardeshi2003@gmail.com>
2025-01-21 19:43:38 +0530

    Add files via upload

---
+++ invoke_current_class_thisMethod.java
@@ -0,0 +1,52 @@
+/* 2.Write java program to invoke current class method using this keyword
+*/
+
+
+package Java_Internship;
+
+class A {
+    
+   void method1() {
+        
+       System.out.println("Method1");
+        
+       // Calling method2 using 'this'
+        
+       this.method2();
+    
+   }
+
+    
+   void method2() {
+        
+       System.out.println("Method2");
+    
+   }
+
+}
+
+public class invoke_current_class_thisMethod 
+{
+    
+    public static void main(String[] args) {
+        
+        // Creating an object of class A
+        
+        A obj = new A();
+        
+        // Invoking method1
+        
+        obj.method1();
+    
+    }
+
+}
+
+/*Output
+
+
+Method1
+
+Method2
+
+*/
---
+++ thiskeyword_argument.java
@@ -0,0 +1,55 @@
+/*1.Write a Java Program  to pass  this keyword as argument in the constructor call.*/
+
+
+package Java_Internship;
+
+class A {
+    
+  A()   {
+        
+        // Passing 'this' to another method in the constructor
+        
+        new B(this);
+    
+  }
+
+    
+  void display() {
+        
+       System.out.println("Method of Class A ");
+   
+  }
+
+}
+
+
+class B {
+    
+  B(A a) {
+       
+        // Calling the method of Class A
+        
+        a.display();
+    
+        }
+
+}
+
+public class thiskeyword_argument
+{
+	
+        public static void main(String[] args) {
+	 
+        // Creating object of Class A
+        
+             new A();
+    
+        }
+
+}
+
+/*Output
+
+Method of class A 
+
+*/
